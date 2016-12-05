bb1d6b1782862bd1078229e0e2ac9875ed638b4b
peturorns <po14@ru.is>
2016-12-05 14:00:16 +0000

    Added new class, computer

---
+++ vika2/computer.cpp
@@ -0,0 +1,14 @@
+#include "computer.h"
+
+Computer::Computer()
+{
+
+}
+
+Computer::Computer(string name, string year, string type, string built)
+{
+    _name = name;
+    _year = year;
+    _type = type;
+    _built = built;
+}
