872381cb50edb23be8c3224ac582840aa2a6b068
Kamilla Guðmundsdóttir <kamilla16@ru.is>
2016-12-16 14:12:35 +0000

    m

---
+++ vika2/Data.h
@@ -0,0 +1,47 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include "Person.h"
+#include "Computer.h"
+#include "PersonsAndComputers.h"
+#include "Facts.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <QSqlDatabase>
+#include <QSql>
+#include <QSqlQuery>
+#include <QSqlRecord>
+#include <QDebug>
+#include <QVariant>
+
+
+class Data
+{
+private:
+
+public:
+    Data();
+    void read(vector<Person>& pers);
+    void readComputer(vector<Computer>& comp);
+    void write(string& firstname, string& sex, string& birth, string& death);
+    void writeComputer(string& name, string& year, string& type, string& built);
+    void remove(string& name, int& type);
+    void edit(string& ID, string& value, string& type);
+    void editComp(string& ID, string& value, string& type);
+    QSqlDatabase m_db;
+    bool openDatabase();
+    vector<Person> sortPersons(string& type);
+    vector<Computer> sortComputer(string& type);
+
+    void getPACjoined(vector<PersonsAndComputers>& pAc);
+    void writePAC(string& pers_id, string& comp_id);
+    bool editPAC(string& ID);
+    void getFact(vector<Facts>& fac);
+
+    bool clear(int& type);
+
+
+};
+
+#endif // DATA_H
---
+++ vika2/Domain.h
@@ -0,0 +1,49 @@
+#ifndef DOMAIN_H
+#define DOMAIN_H
+#include "Person.h"
+#include "PersonsAndComputers.h"
+#include <vector>
+#include <iostream>
+#include "Data.h"
+#include <algorithm>
+
+using namespace std;
+
+class Domain
+{
+private:
+
+public:
+    Domain();
+
+    bool openDatabase();
+
+    bool add(string& firstname, string& sex, string& birth, string& death);
+    bool addComputer(string& name, string& year, string& type, string& built);
+
+    vector<Computer> sortComputer(string& value);
+    vector<Person> sorting(string& input);
+
+    vector<Person> search(string& whattype, string& input);
+    vector<Computer> searchComputer(string& whattype, string& input);
+
+    void edit(vector<Person>& pers, int& line, string& type, string& newValue);
+    void editComputer(vector<Computer>& comp, int& line, string& type, string& newValue);
+
+    vector<Person> list();
+    vector<Computer> computerList();
+
+    bool connectPtoC(string& persID, string& compID);
+    bool editPtoC(string& ID);
+
+    void remove(vector<Person>& pers, int& line);
+    void removeComputer(vector<Computer>& comp, int& line);
+
+    vector<PersonsAndComputers> persAndCompList();
+    string Lower_Ans(string word);
+
+    bool clear(string& type);
+    string funFact();
+};
+
+#endif // DOMAIN_H
